f8825e648517efc91d2ca191fbbad2971e937fae
Bourkupine <30806087+Bourkupine@users.noreply.github.com>
2022-04-05 12:57:23 +0100

    updated driver to include args checks

--- src/DictionaryDriver.java
+++ src/DictionaryDriver.java
@@ -1,13 +1,26 @@
-import java.util.Arrays;
 
 public class DictionaryDriver {
 
     public static void main(String[] args) {
 
+        //first I will check the args, to ensure we have 3
         if (args.length < 3) {
-            System.out.println("error with args");
+            System.out.println("missing args");
+        }
+        if (args.length > 3) {
+            System.out.println("too many args");
         }
 
+        //Here we check to see if args[1] and args[2] are integers
+        try
+        {
+            Integer.parseInt(args[1] + args[2]);
+        } catch (NumberFormatException ex)
+        {
+            System.out.println("args[1] and/or args[2] arent Integers");
+        }
+
+        //create out dictionary
         Dictionary wordlist = new Dictionary(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
 
         System.out.println(wordlist.toString()); //print Dictionary
@@ -29,6 +42,7 @@ public class DictionaryDriver {
         System.out.println("contains ball? " + wordlist.inDictionary("ball")); //checking if exists in Dictionary
         System.out.println("contains dOG? " + wordlist.inDictionary("dOG")); //checking case sensitivity
         System.out.println("contains \"      frog    \"? " + wordlist.inDictionary("      frog    ")); //show trim
+
 
 
 
